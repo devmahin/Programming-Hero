b400cf5e961374395ea498d66766ab427b074c64
devmahin <mahinhowlader123@gmail.com>
2024-06-08 18:46:03 +0600

    agreement and aprtment get post completed

---
+++ Milestone 12/Assignment 12/server_side/index.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const app = express()
+var cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 8000
+// const { Resend } = require('resend')
+// const resend = new Resend(api_key)
+// middleware
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.iagloem.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+// middleware 
+app.use(cors(corsOptions))
+app.use(express.json())
+
+async function run() {
+    const apartmentCollection = client.db("BUILDING_MANAGEMENT").collection("apartment");
+    const agreementCollection = client.db("BUILDING_MANAGEMENT").collection("agreement");
+    try {
+        // apartment data ?
+        app.get("/apartment", async (req, res) => {
+            const result = await apartmentCollection.find().toArray()
+            res.send(result)
+        })
+
+        // agreement data 
+        app.post("/agreement", async (req, res) => {
+            const data = req.body;
+            const result = await agreementCollection.insertOne(data)
+            res.send(result)
+        })
+
+        // Connect the client to the server	(optional starting in v4.7)
+        // Send a ping to confirm a successful connection
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+    }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
